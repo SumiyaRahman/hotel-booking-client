9c1a6b92b2c8ebc600d5b82805f6526f6fe5527f
SumiyaRahman <sumiarahman01739@gmail.com>
2024-12-28 19:50:51 +0600

    reserve fix

--- src/component/Reserve/Reserve.jsx
+++ src/component/Reserve/Reserve.jsx
@@ -6,7 +6,7 @@ import "./Reserve.css";
 const Reserve = () => {
   return (
     <section className="bg-reserve py-10">
-      <div className="flex flex-col lg:flex-row justify-around items-center play-fair text-white text-lg md:text-2xl md:text-3xl gap-6">
+      <div className="flex flex-col lg:flex-row justify-around items-center play-fair text-white text-lg md:text-3xl gap-6">
         <div className="text-center lg:text-left">
           <motion.p
             className="text-center lg:text-left"
